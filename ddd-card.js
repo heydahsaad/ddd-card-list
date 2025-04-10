281fc3edc2887907ec505a1e97e39cab2ca8fa46
hedahsaad <haidahsaad@gmail.com>
2025-04-10 13:29:24 -0400

    remove div for the bar color

--- ddd-card.js
+++ ddd-card.js
@@ -2,8 +2,8 @@
  * Copyright 2025 heydahsaad
  * @license Apache-2.0, see LICENSE for full text.
  */
-import { LitElement, html, css } from "lit";
-import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
+import { html, css } from "lit";
+import { DDDPulseEffectSuper, DDD } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 
 /**
@@ -12,7 +12,7 @@ import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
  * @demo index.html
  * @element ddd-card-list
  */
-export class DddCard extends DDDSuper(I18NMixin(LitElement)) {
+export class DddCard extends DDDPulseEffectSuper(I18NMixin(DDD)) {
 
   static get tag() {
     return "ddd-card";
@@ -58,53 +58,55 @@ export class DddCard extends DDDSuper(I18NMixin(LitElement)) {
         :host {
           display: inline-flex;
           flex-direction:column;
-          /* background: white; */
+          //background: var(--ddd-theme-default-shrineLight); ;
+          /* background-color: var(--ddd-theme-primary, #caccd0); */
           border-radius: 8px;
-          box-shadow: 0 4px 18px rgba(0,0,0,0.6);
+          box-shadow: 0 2px 9px rgba(0,0,0,0.4);
           overflow: hidden;
           max-width: 400px;
-          min-width:300px;
+          min-width: 300px;
         }
 
-        .img {
-          width: 100%;
-          height: 200px;
-          /* object-fit: cover; */
-          margin: 0;
-          padding: 0;
+        img{
+          border-bottom: 8px solid var(--ddd-theme-primary); 
         }
 
-        .title-bar {
+        .title {
           font-size: 1.5rem;
           font-weight: bold;
-          padding: 1rem 1rem 0.5rem;
-          color: #1E407C;
+          color: var(--ddd-theme-default-nittanyNavy);
         }
 
         .description {
-          padding: 0 1rem 1rem;
-          color: black;
-          line-height: 1.4;
+          color: var(--ddd-theme-default-potential70);
+          padding:10px 0px 10px 0px;
         }
 
-        .explore-button {
+        a.explore-button {
           display: block;
-          background: blue; 
-          color: white; /* Changed text color to white for better contrast */
           text-align: center;
           padding: 12px 16px 12px 24px;
           border-radius: 4px;
-          font-weight: 5
-          0;
+          font-weight: 50px;
+          background: var(--ddd-theme-default-beaverBlue);
+          color: var(--ddd-theme-default-roarMaxlight);
         }
 
         .explore-button:hover {
-          background: #15325F;
-          color:white;
+          background: var(--ddd-theme-default-potential75);
+          color:var(--ddd-theme-default-roarMaxlight);
+          text-decoration: none;
         }
 
         .below-img{
-          padding:5px;
+          padding:16px 16px 20px;
+          min-width:100px;
+          min-height: 200px;
+        }
+
+        .card-wording{
+          min-height:250px;
+          /* align-content: center; */
         }
 
     `];
@@ -113,11 +115,12 @@ export class DddCard extends DDDSuper(I18NMixin(LitElement)) {
   // Lit render the HTML
   render() {
     return html`
-      <img src="${this.image}" alt="${this.title || "Campus image"}"/>
-      <div style="padding:5px; background-color:#15325F;"></div>
+        <img src="${this.image}" alt="${this.title || "Campus image"}"/>
       <div class="below-img">
-        <div class="title-bar">${this.title}</div>
-        <div class="description">${this.desc}</div>
+        <div class="card-wording">
+          <div class="title">${this.title}</div>
+          <div class="description">${this.desc}</div>
+        </div>
         <a href="${this.link}" class="explore-button">Explore ></a>
       </div>
     `;
@@ -127,8 +130,83 @@ export class DddCard extends DDDSuper(I18NMixin(LitElement)) {
    * haxProperties integration via file reference
    */
   static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
+    return {
+      type: "element",
+      canScale: true,
+
+      canEditSource: true,
+      gizmo: {
+        title: "Call to action",
+        description: "A simple button with a link to take action.",
+        icon: "image:crop-16-9",
+        color: "orange",
+        tags: ["Layout", "marketing", "button", "link", "url", "design", "cta"],
+        handles: [
+          {
+            type: "link",
+            source: "link",
+            title: "label",
+          },
+        ],
+        meta: {
+          author: "HAXTheWeb core team",
+        },
+      },
+      settings: {
+        configure: [
+          {
+            property: "label",
+            title: "Label",
+            description: "Link label",
+            inputMethod: "textfield",
+            required: true,
+          },
+          {
+            property: "link",
+            title: "Link",
+            description: "Enter a link to any resource",
+            inputMethod: "haxupload",
+            noVoiceRecord: true,
+            noCamera: true,
+            required: true,
+          },
+          {
+            property: "accentColor",
+            title: "Accent Color",
+            description: "An optional accent color.",
+            inputMethod: "colorpicker",
+            icon: "editor:format-color-fill",
+          },
+          {
+            property: "hideIcon",
+            title: "Hide icon",
+            description: "Hide the icon used to accent text",
+            inputMethod: "boolean",
+          },
+        ],
+        advanced: [
+          {
+            property: "icon",
+            title: "Icon",
+            description: "Action link icon",
+            inputMethod: "iconpicker",
+          },
+        ],
+      },
+      saveOptions: {
+        unsetAttributes: ["colors", "element-visible"],
+      },
+      demoSchema: [
+        {
+          tag: "simple-cta",
+          properties: {
+            label: "Click to learn more",
+            link: "https://haxtheweb.org/",
+          },
+          content: "",
+        },
+      ],
+    };
   }
 }
 
